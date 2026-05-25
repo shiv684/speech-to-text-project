b2b3333306431c874560dc121f7b2a7dac9be93d
Shiv684 <shivamravat631@gmail.com>
2026-05-25 12:18:30 +0530

    Day 1 and Day 2 completed

---
+++ backend/server.js
@@ -0,0 +1,55 @@
+
+const express=require("express");
+const cors=require("cors");
+const multer=require("multer");
+
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const storage=multer.diskStorage({
+
+    // file kha save hogi
+    destination:(req,file,cb) =>{
+        cb(null,"uploads/");
+
+    },
+
+    // file ka kya naam hoga
+
+    filename:(req,file,cb)=>{
+        cb(null,Date.now()+"-"+file.orginalname);
+    }
+
+})
+
+// multer activate
+
+const upload=multer({storage});
+
+const PORT=3000;
+
+// home route
+
+app.get("/",(req,res)=>{
+    res.send("backend is runing")
+
+});
+
+// upload route
+
+app.post("/upload",upload.single("audio"),(req,res)=>{
+    res.json({
+        "message":"upload successfully",
+        file:req.file
+
+    });
+});
+
+app.listen(PORT,()=>{
+    console.log(`server is running  on port ${PORT}`);
+
+})
